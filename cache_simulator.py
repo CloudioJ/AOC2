c52eecd5a7e041c0632c55e6323f6372de530b99
Claudio J <48624347+CloudioJ@users.noreply.github.com>
2024-03-08 00:39:23 -0300

    Update final - maybe | LRU e FIFO ok

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import random as rd
+from collections import deque
 
 # Começa uma classe para os atributos de entrada
 class Attributes:
@@ -54,59 +55,8 @@ class Misses:
 misses = Misses()
 full = 0
 
-#cria uma lista duplamente encadeada, por causa dos blocos, pra armazenar os indices e os blocos
-class Node:
-    def __init__(self, index):
-        self.index = index
-        self.next = None
-        self.prev = None
-
-cache_list = [[None] * att.assoc for _ in range(att.nsets)]
-
-class Sub:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def add(self, index):
-        new_node = Node(index)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-
-    def move(self, node):
-        if node == self.tail:
-            return
-        if node == self.head:
-            self.head = node.next
-            self.head.prev = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-        node.next = None
-        node.prev = self.tail
-        self.tail.next = node
-        self.tail = node
-
-    def remove(self):
-        if self.head is None:
-            return None
-        removed = self.head.index
-        self.head = self.head.next
-        if self.head is not None:
-            self.head.prev = None
-        else:
-            self.tail = None
-        return removed
-
 def main():
-    
-    sub_cache = Sub()
-    full = 0
+
     # Verifica se o número de argumentos está correto
     if (len(sys.argv) != 7):
         print("Usage: python cache_simulator.py <nsets> <bsize> <assoc> <sub> <flag> <tracefile>")
@@ -115,24 +65,16 @@ def main():
     # Inicializa os vetores de valores e tags da cache com o tamanho da associatividade vezes o número de conjuntos
     cache_val = [[0] * att.assoc for _ in range(att.nsets)]
     cache_tag = [[0] * att.assoc for _ in range(att.nsets)]
-
+    
     # Calcula o número de bits de offset, index e tag
     offset_bits = int(np.log2(att.bsize))
     index_bits = int(np.log2(att.nsets))
     tag_bits = 32 - offset_bits - index_bits
 
-    # print(offset_bits)  
-    # print(index_bits)
-    # print(tag_bits)
-
     # Abre o arquivo de entrada e lê os dados em formato big endian
     file = open(att.file, "rb")
     data = np.fromfile(file, dtype=">u2")
-    
-    # print(data.size)
-    # print(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])
 
-    sub_cache = Sub()
     # Itera sobre os dados do arquivo de entrada
     for i in range(0, data.size):   
 
@@ -145,18 +87,26 @@ def main():
             index = (data[i] >> offset_bits) & ((1 << index_bits) - 1)
 
             # Vê onde vai ser colocado o dado na cache
-            cache_val, cache_tag = cache_placement(index, tag, cache_val, cache_tag, sub_cache)
+            cache_val, cache_tag = cache_placement(index, tag, cache_val, cache_tag)
             att.addCounter()
 
     # print(f"Counter: {att.counter}")
 
-def cache_placement(index, tag, cache_val, cache_tag, sub_cache):
+def split_list(a_list, index):
+    return a_list[:index], a_list[index:]
+
+def rotate_list(a_list):
+    a_list = deque(a_list)
+    a_list.rotate(-1)
+    return list(a_list)
+
+def cache_placement(index, tag, cache_val, cache_tag):
 
     # Flag serve para não colocar o mesmo dado na cache mais de uma vez
     flag = 0
 
     # Checa toda a cache pra ver se esta cheia
-    full = all(cache_val)
+    full = all(cache_val[index])
 
     # Itera baseado no numero de associatividade
     for i in range(att.assoc):
@@ -166,15 +116,18 @@ def cache_placement(index, tag, cache_val, cache_tag, sub_cache):
             cache_val[index][i] = 1
             cache_tag[index][i] = tag
             flag = 1
-            sub_cache.add(index)
 
         # Se ja existir o dado então é hit, transformando o flag em 1 pra ele não passar no resto do código
         else:
             if cache_tag[index][i] == tag and flag != 1:
                 att.addHits()
                 flag = 1
-                if att.sub == 'L':
-                    sub_cache.move(sub_cache.head)
+                # Se for LRU então divide a lista em duas e rotaciona a segunda parte, tornando o hit o último da lista
+                if att.sub == 'L' and full:
+                    part1, part2 = split_list(cache_tag[index], i - 1)
+                    part2 = deque(part2)
+                    part2.rotate(-1)
+                    cache_tag[index] = part1 + list(part2)
 
     # Se não tiver sido hit e também não foi um miss compulsório então foi miss de conflito ou de capacidade
     if flag == 0:
@@ -185,37 +138,20 @@ def cache_placement(index, tag, cache_val, cache_tag, sub_cache):
             aux = rd.randint(0, 20) % att.assoc
             cache_tag[index][aux] = tag
         
-        elif full and att.sub == 'L':  # Miss de capacidade pro LRU
+        elif full and att.sub == 'L' or att.sub == 'F':  # Miss de capacidade pro LRU e FIFO
             misses.addCapacity()
-            removed_index = sub_cache.head.index
-            sub_cache.remove()  # Remove o primeiro bloco
-            cache_val[index][removed_index] = 1  # Deixa um bit válido pro novo bloco
-            cache_tag[index][removed_index] = tag  # Deixa o tag pro novo bloco
-            sub_cache.add(index)  # Adiciona o novo bloco no final da lista
+            cache_tag[index][0] = tag
+            cache_tag[index] = rotate_list(cache_tag[index])
 
-        elif full and att.sub == 'F':  # Miss de capacidade pro FIFO
-            misses.addCapacity()
-            removed_index = sub_cache.head.index
-            sub_cache.remove() 
-            cache_val[index][removed_index] = 1 
-            cache_tag[index][removed_index] = tag 
-            sub_cache.add(index)
-
-        else:  # Miss de conflito
+        else:  # Miss de conflito LRU e FIFO
             misses.addConflict()
             if att.sub == 'R':
                 aux = rd.randint(0, 20) % att.assoc
                 cache_tag[index][aux] = tag
 
-            elif att.sub == 'L':
-                sub_cache.add(index)  # Adiciona o novo bloco no final da lista
-
-            elif att.sub == 'F':
-                removed_index = sub_cache.head.index
-                sub_cache.remove() 
-                cache_val[index][removed_index] = 1  
-                cache_tag[index][removed_index] = tag  
-                sub_cache.add(index) 
+            elif att.sub == 'L' or att.sub == 'F':
+                cache_tag[index][0] = tag
+                cache_tag[index] = rotate_list(cache_tag[index])
 
     return cache_val, cache_tag
 
